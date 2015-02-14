106c58437435e0b79df040fc7cb4921d4b0cecc9
AnHaNguyen <a0113038@nus.edu.sg>
2015-02-14 16:07:56 +0800

    Tasks class added

---
+++ src/udo/storage/Tasks.java
@@ -0,0 +1,104 @@
+package udo.storage;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class Tasks {
+	//class defines tasks objects
+	private String taskType;
+	private String content;
+	private GregorianCalendar start;
+	private GregorianCalendar end;
+	private GregorianCalendar reminder;
+	private String label;
+	private boolean priority;
+	private boolean done;
+	
+	//constructor
+	public Tasks(String taskType, String content, GregorianCalendar start, GregorianCalendar end, 
+			GregorianCalendar reminder, String label, boolean priority){
+		this.taskType = taskType;
+		this.content = content;
+		this.start = start;
+		this.end = end;
+		this.reminder = reminder;
+		this.label = label;
+		this.priority = priority;
+		this.done = false;
+	}
+	
+	public String getType(){
+		return taskType;
+	}
+	
+	public String getContent(){
+		return content;
+	}
+
+	public GregorianCalendar getStart(){
+		return start;
+	}
+
+	public GregorianCalendar getEnd(){
+		return end;
+	}
+	
+	public GregorianCalendar getReminder(){
+		return reminder;
+	}
+	
+	public String getLabel(){
+		return label;
+	}
+
+	public boolean isPriority(){
+		return priority;
+	}
+	
+	public boolean isDone(){
+		return done;
+	}
+
+	public void setType(String type){
+		this.taskType = type;
+	}
+	
+	public void setContent(String content){
+		this.content = content;
+	}
+	
+	public void setStart(GregorianCalendar start){
+		this.start = start;
+	}
+	
+	public void setEnd(GregorianCalendar end){
+		this.end = end;
+	}
+	
+	public void setReminder(GregorianCalendar reminder){
+		this.reminder = reminder;
+	}
+	
+	public void setLabel(String label){
+		this.label = label;
+	}
+	
+	public void setPriority(){
+		this.priority = !(this.priority);
+	}
+	
+	public void setDone(){
+		this.done = !(this.done);
+	}
+	
+	public String toString(){
+		String finalString = "";
+		String startDate = start.get(Calendar.DAY_OF_MONTH) + "/" + start.get(Calendar.MONTH) + "/" + start.get(Calendar.YEAR);
+		String endDate = end.get(Calendar.DAY_OF_MONTH) + "/" + end.get(Calendar.MONTH) + "/" + end.get(Calendar.YEAR);
+		String reminderDate = reminder.get(Calendar.DAY_OF_MONTH) + "/" + reminder.get(Calendar.MONTH) + "/" + reminder.get(Calendar.YEAR);
+		finalString += taskType + "     " + content + "      " + startDate + "     " + endDate + "    " + reminderDate + "     " + label 
+				+ "     ";
+		return finalString;
+	}
+}
+
