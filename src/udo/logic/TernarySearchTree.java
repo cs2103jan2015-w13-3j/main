9c4599d723a53ec9249a23189386f8f01de2d84e
ndt93 <thiennguyen1993@gmail.com>
2015-02-15 22:03:59 +0800

    Add search prefix method to ternary search tree

--- src/udo/logic/TernarySearchTree.java
+++ src/udo/logic/TernarySearchTree.java
@@ -1,5 +1,7 @@
 package udo.logic;
 
+import java.util.List;
+
 public class TernarySearchTree {
     public static class Node {
         private char myChar;
@@ -14,20 +16,20 @@ public class TernarySearchTree {
     
     private Node root = null;
     
-    private Node add(String s, Integer pos, Node node) {
+    private Node add(String s, Integer index, Node node) {
         if (node == null) {
-            node = new Node(s.charAt(pos), false);
+            node = new Node(s.charAt(index), false);
         }
         
-        if (s.charAt(pos) < node.myChar) {
-            node.left = add(s, pos, node.left);
-        } else if (s.charAt(pos) > node.myChar) {
-            node.right = add(s, pos, node.right);
+        if (s.charAt(index) < node.myChar) {
+            node.left = add(s, index, node.left);
+        } else if (s.charAt(index) > node.myChar) {
+            node.right = add(s, index, node.right);
         } else {
-            if (pos + 1 == s.length()) {
+            if (index + 1 == s.length()) {
                 node.isWordEnd = true;
             } else {
-                node.center = add(s, pos + 1, node.center); 
+                node.center = add(s, index + 1, node.center); 
             }
         }
         
@@ -45,6 +47,43 @@ public class TernarySearchTree {
         }
         
         root = add(s, 0, root);
+    }
+    
+    public List<String> searchPrefix(String prefix, Integer numWords) {
+        int index = 0;
+        Node curNode = root;
+        
+        while (curNode != null) {
+            if (index == prefix.length()) {
+                return searchTree(curNode, numWords);
+            }
+            
+            if (prefix.charAt(index) < curNode.myChar) {
+                curNode = curNode.left;
+            } else if (prefix.charAt(index) > curNode.myChar) {
+                curNode = curNode.right;
+            } else {
+                index++;
+                curNode = curNode.center;
+            }
+        }
+        
+        return null;
+    }
+    
+    public List<String> searchPrefix(String prefix) {
+        return searchPrefix(prefix, null);
+    }
+    
+    /**
+     * Find words in the tree rooted at some node
+     * @param node the root of the tree for the search
+     * @param numWords maximum number of words to search for or
+     *                 search for all words if this is null
+     * @return list of found words
+     */
+    private List<String> searchTree(Node node, Integer numWords) {
+        return null;
     }
 
     public static void main(String[] args) {
