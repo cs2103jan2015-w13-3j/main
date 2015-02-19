2d1068599f12adf5d1c421cfbd2145b53c8ba1f7
Sharmine <slxmlow@gmail.com>
2015-02-19 21:07:55 +0800

    initial layout of gui

--- src/udo/gui/GUI.java
+++ src/udo/gui/GUI.java
@@ -1,10 +1,115 @@
 package udo.gui;
 
-public class GUI {
+import java.io.IOException;
+import java.util.ArrayList;
 
+import udo.storage.Tasks;
+import udo.gui.view.HomeController;
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class GUI extends Application{
+    
+    private Stage primaryStage;
+    private BorderPane rootLayout;
+    private static ObservableList<Tasks> taskData;
+    
+    /**
+     * Constructor
+     */
+    public GUI() {
+        
+        /*For testing purposes
+        ArrayList<Tasks> testList = new ArrayList<Tasks>();
+        display(testList);
+         */
+    }
+
+    
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("JustU");
+
+        initRootLayout();
+
+        showOverview();
+    }
+
+    /**
+     * Initializes the root layout.
+     */
+    public void initRootLayout() {
+        try {
+            // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(GUI.class.getResource("view/RootLayout.fxml"));
+            rootLayout = (BorderPane) loader.load();
+
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Shows overview inside the root layout.
+     */
+    public void showOverview() {
+        try {
+            // Load person overview.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(GUI.class.getResource("view/Home.fxml"));
+            AnchorPane homeOverview = (AnchorPane) loader.load();
+
+            // Set person overview into the center of root layout.
+            rootLayout.setCenter(homeOverview);
+            
+            // Give the controller access to the main app.
+            HomeController controller = loader.getController();
+            controller.setMainApp(this);
+         
+                       
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    /**
+     * @param Arraylist<Tasks>
+     */
+    public static void display(ArrayList<Tasks> testingInputTaskList) { 
+        taskData = FXCollections.observableArrayList(testingInputTaskList);
+    }
+
+
+    /**
+     * Returns the data as an observable list of Task 
+     * @return
+     */
+    public ObservableList<Tasks> getTaskData() {
+        return taskData;
+    }
+    
+    
+    /**
+     * Returns the main stage.
+     * @return
+     */
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
-
+        launch(args);
     }
 
 }
---
+++ src/udo/gui/view/HomeController.java
@@ -0,0 +1,60 @@
+package udo.gui.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.event.ActionEvent;
+
+import udo.gui.GUI;
+import udo.storage.Tasks;
+
+public class HomeController {
+    @FXML
+    private TableView<Tasks> TaskTable;
+    @FXML
+    private TableColumn<Tasks, String> displayIndexColumn;
+    @FXML
+    private TableColumn<Tasks, String> taskNameColumn;
+    @FXML
+    private TextField inputBox;
+    
+    // Reference to the main application.
+    private GUI gui;
+    
+    public HomeController() {
+    }
+
+    /**
+     * Initializes the controller class. This method is automatically called
+     * after the fxml file has been loaded.
+     */
+    @FXML
+    private void initialize() {
+        // Initialize the Task table with the two columns.
+        //displayIndexColumn.setCellValueFactory(cellData -> cellData.getValue().displayIndexProperty());
+        //taskNameColumn.setCellValueFactory(cellData -> cellData.getValue().taskNameProperty());
+        
+    }
+    
+    @FXML
+    private void handleReturnKey(ActionEvent event) {
+        // to pass string to Logic Class
+        String text = inputBox.getText();
+        System.out.println(text);
+        inputBox.clear();
+    }
+    
+    /**
+     * Is called by the main application to give a reference back to itself.
+     * 
+     * @param gui
+     */
+    public void setMainApp(GUI gui) {
+        this.gui = gui;
+
+        // Add observable list data to the table      
+        TaskTable.setItems(gui.getTaskData());  
+    }
+    
+}
