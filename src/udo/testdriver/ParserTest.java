c79a724fbafc62811642d144c2345cd66840e361
ndt93 <thiennguyen1993@gmail.com>
2015-03-16 11:31:12 +0800

    Add test skeleton code

--- src/udo/testdriver/ParserTest.java
+++ src/udo/testdriver/ParserTest.java
@@ -5,6 +5,13 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class ParserTest {
+    private static final String testsDir = "res/tests/parser";
+    private String[] inputs;
+    private String[] expected;
+
+    public ParserTest() {
+        
+    }
 
     @Test
     public void test() {
