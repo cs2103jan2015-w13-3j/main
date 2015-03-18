4f63d707ba94fef92d955a00d6b42170dd9bb8e6
AnHaNguyen <a0113038@nus.edu.sg>
2015-03-18 20:30:19 +0800

    add start time comparator

---
+++ src/udo/storage/StartTimeComparator.java
@@ -0,0 +1,11 @@
+package udo.storage;
+import java.util.Comparator;
+import java.util.GregorianCalendar;
+public class StartTimeComparator implements Comparator<Task> {
+	@Override
+	public int compare(Task task1, Task task2) {
+		GregorianCalendar start1 = task1.getStart();
+		GregorianCalendar start2 = task2.getStart();
+		return start1.compareTo(start2);
+	}
+}
