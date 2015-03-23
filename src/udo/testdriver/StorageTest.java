9d0895ff804e73691e20ca16ef820cc5826f5d83
AnHaNguyen <a0113038@nus.edu.sg>
2015-03-24 03:40:08 +0800

    add more test cases to StorageTest

--- src/udo/testdriver/StorageTest.java
+++ src/udo/testdriver/StorageTest.java
@@ -23,6 +23,8 @@ public class StorageTest {
 	public void initialize(){
 		clearFile(storageFile);
 		st = new Storage();
+		taskList.clear();
+		doneTasks.clear();
 	}
 	
 	public void clearFile(String fileName) {
@@ -37,6 +39,18 @@ public class StorageTest {
 		}
 	}
 	
+	private void addDummyTasks() {
+		Task task1 = new Task(TaskType.TODO, "fighting", null,null, null,
+				120, new GregorianCalendar(2011,01,02), "personal", false, false);
+		Task task2 = new Task(TaskType.EVENT, "reading books", null, new GregorianCalendar(2006,03,01), new GregorianCalendar(2005,04,01),
+				0, null, "leisure", false, false);
+		
+		ArrayList<Task> temp = new ArrayList<Task>();
+		temp.add(task1);
+		temp.add(task2);
+		st.add(temp);
+	}
+
 	@Test
 	public void testAdd() {	
 		initialize();
@@ -52,15 +66,14 @@ public class StorageTest {
 		st.add(task1);
 		st.add(task2);
 	
-		
-		assertEquals(task0, st.query().get(st.query().size() - 3));
-		assertEquals(task1, st.query().get(st.query().size() - 2));
-		assertEquals(task2, st.query().get(st.query().size() - 1));
-
+		taskList.add(task0);
+		taskList.add(task1);
+		taskList.add(task2);
+		assertEquals(taskList, st.query());
 	}
 	
 	@Test
-	public void testDel() {
+	public void testDelete() {
 		initialize();
 		assertEquals(false, st.delete(0));
 		st.add(new Task(TaskType.DEADLINE, "meeting", new GregorianCalendar(2005,01,01), null, null,
@@ -81,12 +94,13 @@ public class StorageTest {
 		assertEquals(false, st.delete(3));
 		assertEquals(true, st.delete(0));
 		
-		assertEquals(task2,st.query().get(0));
-		assertEquals(task1,st.query().get(1));
+		taskList.add(task2);
+		taskList.add(task1);
+		assertEquals(taskList, st.query());
 	}
 
 	@Test
-	public void TestMod(){
+	public void TestModify(){
 		initialize();
 		assertEquals(false, st.modify(0, new Task()));
 		
@@ -97,7 +111,90 @@ public class StorageTest {
 		
 		st.add(task0);
 		assertEquals(true, st.modify(0, task1));
-		assertEquals(task1, st.query().get(0));
+		taskList.add(task1);
+		assertEquals(taskList, st.query());
 		
+	}
+
+	@Test
+	public void TestQuery(){
+		initialize();
+		assertEquals(new ArrayList<Task>(), st.query());
+		Task task0 = new Task(TaskType.DEADLINE, "meeting", new GregorianCalendar(2005,01,01), null, null,
+				0, new GregorianCalendar(2005,01,02), "work",true, false);
+		st.add(task0);
+		taskList.add(task0);
+		assertEquals(taskList, st.query());
+	}
+	
+	@Test
+	public void TestUndo(){
+		initialize();
+		Task task0 = new Task(TaskType.DEADLINE, "meeting", new GregorianCalendar(2005,01,01), null, null,
+				0, new GregorianCalendar(2005,01,02), "work",true, false);
+		Task task1 = new Task(TaskType.TODO, "fighting", null,null, null,
+				120, new GregorianCalendar(2011,01,02), "personal", false, false);
+		
+		assertEquals(false,st.undo());
+		st.add(task0);
+		assertEquals(true, st.undo());
+		assertEquals(new ArrayList<Task>(), st.query());
+		assertEquals(false, st.undo());					//cannot undo multiple times
+		
+		st.add(task0);
+		st.modify(0, task1);
+		assertEquals(true, st.undo());
+		taskList.add(task1);
+		assertEquals(taskList, st.query());
+	
+		st.delete(0);
+		assertEquals(true, st.undo());
+		assertEquals(taskList, st.query());
+	}
+
+	@Test
+	public void TestAddDummy(){
+		initialize();
+		assertEquals(false, st.add(new ArrayList<Task>()));
+	
+		addDummyTasks();
+		for (int i = 0; i < st.query().size(); i++){
+			assertEquals(new Integer(1), st.query().get(i).getGroupId());
+			assertEquals(new Integer(1), st.query().get(i).getGroupId());
+		}
+	
+		addDummyTasks();
+		for (int i = 2; i < st.query().size(); i++){
+			assertEquals(new Integer(2), st.query().get(i).getGroupId());
+			assertEquals(new Integer(2), st.query().get(i).getGroupId());
+		}
+	}
+	
+	@Test
+	public void TestConfirm(){
+		initialize();
+		addDummyTasks();
+		
+		assertEquals(false, st.confirm(-1));
+		assertEquals(false, st.confirm(3));
+		assertEquals(true, st.confirm(0));
+		Task task1 = new Task(TaskType.TODO, "fighting", null,null, null,
+				120, new GregorianCalendar(2011,01,02), "personal", false, false);
+		Task task2 = new Task(TaskType.EVENT, "reading books", null, new GregorianCalendar(2006,03,01), new GregorianCalendar(2005,04,01),
+				0, null, "leisure", false, false);
+		task1.setIndex(0);
+		task1.setGroupId(0);
+		assertEquals(task1, st.query().get(0));
+	
+		addDummyTasks();
+		task2.setIndex(2);
+		task2.setGroupId(1);
+		assertEquals(task2, st.query().get(2));
+		
+		assertEquals(true, st.confirm(2));
+		
+		task2.setIndex(1);
+		task2.setGroupId(0);
+		assertEquals(task2, st.query().get(1));
 	}
 }
