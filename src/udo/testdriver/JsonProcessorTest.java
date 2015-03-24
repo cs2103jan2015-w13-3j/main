339a87bed386f6cd8652e11d2baa1dc01d77e26e
bihuutue <bihuutue@gmail.com>
2015-03-24 17:55:27 +0800

    Hotfix #71

--- src/udo/testdriver/JsonProcessorTest.java
+++ src/udo/testdriver/JsonProcessorTest.java
@@ -22,7 +22,7 @@ public class JsonProcessorTest {
 	public static final String expectedFile = "res/tests/storage/expected.txt";
 	public static final String inputFile = "res/tests/storage/input.txt";
 	private static ArrayList<Task> taskList = new ArrayList<Task>();
-	
+
 	public void clearFile(String fileName) {
 		try {
 			FileWriter fw = new FileWriter(fileName);
@@ -34,20 +34,20 @@ public class JsonProcessorTest {
 			ex.printStackTrace();
 		}
 	}
-	
+
 	//code reuse from Thien
 	public String readFile(String filename) {
-        String result = null;
+		String result = null;
 
-        try {
-            result = new String(Files.readAllBytes(Paths.get(filename)));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        
-        return result;
-    }
-	 
+		try {
+			result = new String(Files.readAllBytes(Paths.get(filename)));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return result;
+	}
+
 	@Test
 	public void writeJsonTest() {
 		clearFile(storageFile);
@@ -64,17 +64,24 @@ public class JsonProcessorTest {
 
 	@Test
 	public void readJsonTest() {
-		ArrayList<Task> testList = JsonProcessor.readJson(inputFile);
-		taskList.clear();
-		taskList.add(new Task(TaskType.DEADLINE, "meeting", new GregorianCalendar(2005,01,01), null, null,
-				0, new GregorianCalendar(2005,01,02), "work",true, false));
-		taskList.add(new Task(TaskType.TODO, "fighting", null,null, null,
-				120, new GregorianCalendar(2011,01,02), "personal", false, false));
-		taskList.add(new Task(TaskType.EVENT, "reading books", null, new GregorianCalendar(2006,03,01), new GregorianCalendar(2005,04,01),
-				0, null, "leisure", false, false));
-		assert testList.size() == taskList.size();
-		for (int i = 0; i < taskList.size(); i++){
-			assertEquals(testList.get(i).toString(), taskList.get(i).toString());
+		ArrayList<Task> testList;
+		try {
+			testList = JsonProcessor.readJson(inputFile);
+
+			taskList.clear();
+			taskList.add(new Task(TaskType.DEADLINE, "meeting", new GregorianCalendar(2005,01,01), null, null,
+					0, new GregorianCalendar(2005,01,02), "work",true, false));
+			taskList.add(new Task(TaskType.TODO, "fighting", null,null, null,
+					120, new GregorianCalendar(2011,01,02), "personal", false, false));
+			taskList.add(new Task(TaskType.EVENT, "reading books", null, new GregorianCalendar(2006,03,01), new GregorianCalendar(2005,04,01),
+					0, null, "leisure", false, false));
+			assert testList.size() == taskList.size();
+			for (int i = 0; i < taskList.size(); i++){
+				assertEquals(testList.get(i).toString(), taskList.get(i).toString());
+			}
+		} catch (Exception e) {
+			//System.out.println(e);
+			System.out.println(e);
 		}
 	}
 }
